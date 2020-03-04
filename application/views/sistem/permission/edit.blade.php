c84c983ecb6a922bb29b5c55b2fa9ed5c1be6861
lathiifas <ajisanthoshol@gmail.com>
2020-03-04 22:50:36 +0700

    permission

--- application/views/sistem/permission/edit.blade.php
+++ application/views/sistem/permission/edit.blade.php
@@ -1,147 +1,117 @@
-<div class="content-wrap">
-    <div class="main">
-        <!-- breadcrum -->
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-8 p-0">
-                    <div class="page-header">
-                        <div class="page-title">
-                            <h1>{{ $title }}</h1>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 p-0">
-                    <div class="page-header">
-                        <div class="page-title">
-                            <ol class="breadcrumb text-right">
-                                <li><a href="#">Sistem</a></li>
-                                <li class="active">{{ $title }}</li>
-                                <li class="active">Edit</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                </div>
-                <!-- akhir breadcrum -->
-                <div class="main-content">
-                    <!-- /# row -->
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="page-breadcrumb">
+    <div class="row">
+        <div class="col-7 align-self-center">
+            <h4 class="page-title text-truncate text-dark font-weight-medium mb-1">{{ $title }}
+            </h4>
+            <div class="d-flex align-items-center">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb m-0 p-0">
+                        <li class="breadcrumb-item"><a href="index.html" class="text-muted">Sistem</a></li>
+                        <li class="breadcrumb-item" aria-current="page"><a href="index.html"
+                                class="text-muted">{{ $title }}</a></li>
+                        <li class="breadcrumb-item text-muted active" aria-current="page">Edit</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+        <div class="col-5 align-self-center">
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="container-fluid">
+    {{-- notif wajib ada di setiap halaman admin kecuali delete--}}
+    @include('template/notif')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
                     <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card alert">
-                                <div class="card-header">
-                                    <h4>Edit</h4>
-                                    <div class="card-header-right-icon">
-                                        <ul>
-                                            <a href="{{ site_url('sistem/permission') }}" type="button"
-                                                class="btn btn-default m-b-10 m-l-5">Kembali</a>
-                                        </ul>
+                        <div class="col-lg-10">
+                            <h4 class="card-title">Edit Data</h4>
+                        </div>
+                        <div class="col-lg-2">
+                            <div class="text-right">
+                                <a href="{{ site_url('sistem/permission') }}" type="submit"
+                                    class="btn btn-primary">Kembali</a>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <form action="{{ site_url('sistem/permission/edit_process') }}" method="post">
+                                <input type="hidden" name="role_id" value="{{ $result['role_id'] }}">
+                                <table class="table">
+                                    <thead class="bg-primary text-white">
+                                        <tr>
+                                            <th class="text-align text-center" width="10%"> <input type="checkbox"
+                                                    id="checked-all-menu" class="checked-all-menu"> Semua</th>
+                                            <th class="text-align text-center" width="50%">Judul Navigasi</th>
+                                            <th class="text-align text-center" width="10%">Create</th>
+                                            <th class="text-align text-center" width="10%">Read</th>
+                                            <th class="text-align text-center" width="10%">Update</th>
+                                            <th class="text-align text-center" width="10%">Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {!! $list_menu !!}
+                                    </tbody>
+                                </table>
+                                <div class="col-lg-12">
+                                    <div class="text-right">
+                                        <button type="submit" class="btn btn-success m-b-10 m-l-5"> Simpan</button>
+                                        <button type="reset" class="btn btn-secondary m-b-10 m-l-5"> Reset</button>
                                     </div>
                                 </div>
-
-                                {{-- notif --}}
-                                @include('template/notif')
-
-                                <hr>
-                                <div class="card-body" style="margin-top:20px">
-                                    <div class="card-content">
-                                        <div class="main">
-                                            <div class="horizontal-form-elements">
-                                                <form class="form-horizontal"
-                                                    action="{{ site_url('sistem/permission/edit_process') }}"
-                                                    method="post">
-                                                    <input type="hidden" name="role_id" value="{{ $result['role_id'] }}">
-                                                    <table class="table table-responsive table-striped">
-                                                        <thead>
-                                                            <tr>
-                                                                <th  class="text-align text-center" width="10%">  <input type="checkbox" id="checked-all-menu" class="checked-all-menu">  Semua</th>
-                                                                <th class="text-align text-center" width="50%">Judul Navigasi</th>
-                                                                <th class="text-align text-center" width="10%">Create</th>
-                                                                <th class="text-align text-center" width="10%">Read</th>
-                                                                <th class="text-align text-center" width="10%">Update</th>
-                                                                <th class="text-align text-center" width="10%">Delete</th>
-                                                            </tr>
-                                                        </thead>
-                                                        <tbody>
-                                                           {!! $list_menu !!}
-                                                        </tbody>
-                                                    </table>
-                                                    <hr>
-                                                    <div class="row">
-                                                        <div class="col-lg-6">
-                                                            <div class="form-group">
-                                                                <div class="col-lg-12">
-                                                                    <label class="control-label"><b> Created by
-                                                                        </b></label>
-                                                                </div>
-                                                                <div class="col-lg-12">
-                                                                    <!-- <label
-                                                                        class="control-label">{{ $result['mdb_name'] }}</label> -->
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-lg-6">
-                                                            <div class="form-group">
-                                                                <div class="col-lg-12">
-                                                                    <label class="control-label"><b> Date update
-                                                                        </b></label>
-                                                                </div>
-                                                                <div class="col-lg-12">
-                                                                    <label
-                                                                        class="control-label">{{ $result['mdd'] }}</label>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <hr>
-                                                    <div class="card-footer text-right">
-                                                        <button type="submit"
-                                                            class="btn btn-primary btn-rounded m-b-10 m-l-5">Simpan</button>
-                                                        <button type="reset"
-                                                            class="btn btn-dark btn-rounded m-b-10 m-l-5">Reset</button>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            </form>
+                            <div class="text-right">
+                                @if (isset($pagination))
+                                <ul class="pagination pagination-sm">
+                                    <li class="page-item"><a class="page-link" href="#">{!! $pagination !!}</a>
+                                    </li>
+                                </ul>
+                                @endif
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
 
+@push('ext_js')
+<script>
+    $(document).ready(function () {
+        $(".select2-single").select2({
+            width: '100%',
+            containerCssClass: ':all:'
+        });
+    });
+</script>
 
-            @push('ext_js')
-            <script>
-                $(document).ready(function () {
-                    $(".select2-single").select2({
-                        placeholder: placeholder,
-                        width: '100%',
-                        containerCssClass: ':all:'
-                    });
-                });
-            </script>
-
-            <script type="text/javascript">
-                $(function () {
-                    $(".checked-all").click(function () {
-                        var status = $(this).is(":checked");
-                        if (status === true) {
-                            $(".r" + $(this).val()).prop('checked', true);
-                        } else {
-                            $(".r" + $(this).val()).prop('checked', false);
-                        }
-                    });
-                    $(".checked-all-menu").click(function () {
-                        var status = $(this).is(":checked");
-                        if (status === true) {
-                            $(".r-menu").prop('checked', true);
-                        } else {
-                            $(".r-menu").prop('checked', false);
-                        }
-                    });
-                    $(".select-2").select2();
-                });
-            </script>
-            @endpush
+<script type="text/javascript">
+    $(function () {
+        $(".checked-all").click(function () {
+            var status = $(this).is(":checked");
+            if (status === true) {
+                $(".r" + $(this).val()).prop('checked', true);
+            } else {
+                $(".r" + $(this).val()).prop('checked', false);
+            }
+        });
+        $(".checked-all-menu").click(function () {
+            var status = $(this).is(":checked");
+            if (status === true) {
+                $(".r-menu").prop('checked', true);
+            } else {
+                $(".r-menu").prop('checked', false);
+            }
+        });
+        $(".select-2").select2();
+    });
+</script>
+@endpush
--- application/views/sistem/permission/index.blade.php
+++ application/views/sistem/permission/index.blade.php
@@ -1,116 +1,108 @@
-<div class="content-wrap">
-        <div class="main">
-            <!-- breadcrum -->
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-8 p-0">
-                        <div class="page-header">
-                            <div class="page-title">
-                            <h1>{{ $title }}</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 p-0">
-                        <div class="page-header">
-                            <div class="page-title">
-                                <ol class="breadcrumb text-right">
-                                    <li><a href="#">Sistem</a></li>
-                                    <li class="active">{{ $title }}</li>
-                                </ol>
-                            </div>
-                        </div>
-                    </div>
-                <div>
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="page-breadcrumb">
+    <div class="row">
+        <div class="col-7 align-self-center">
+            <h4 class="page-title text-truncate text-dark font-weight-medium mb-1">{{ $title }}
+            </h4>
+            <div class="d-flex align-items-center">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb m-0 p-0">
+                        <li class="breadcrumb-item"><a href="index.html" class="text-muted">Sistem</a></li>
+                        <li class="breadcrumb-item text-muted active" aria-current="page">{{ $title }}</li>
+                    </ol>
+                </nav>
             </div>
-            <!-- akhir breadcrum -->
-            <div class="main-content">
-    
-            <!-- /# row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card alert">
-                        <div class="card-header">
-                            <h4>Daftar Hak Akses User</h4>
-                            <div class="card-header-right-icon">
-                            </div>
-                            <hr>
+        </div>
+        <div class="col-5 align-self-center">
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="container-fluid">
+    {{-- notif wajib ada di setiap halaman admin kecuali delete--}}
+    @include('template/notif')
+    <!-- ============================================================== -->
+    <!-- Start Page Content -->
+    <!-- ============================================================== -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="row">
+                            <h4 class="card-title col-lg-12">Daftar Hak Role</h4>
                         </div>
-                        <div class="card-body" style="margin-top:20px">
-                            <div class="card-content">
-                                    <div class="horizontal-form-elements">
-                                            <form class="form-horizontal">
-                                                <div class="row">
-                                                    <div class="col-lg-6">
-                                                        <div class="form-group">
-                                                            <label class="col-sm-3 control-label">Group</label>
-                                                            <div class="col-sm-9">
-                                                            <select name="nav_id" id="single" class="form-control select2-single">
-                                                            <option value='0'>Tidak ada</options>
-                                                                @foreach ($rs_group as $group)
-                                                                <option value="{{ $group['group_id'] }}">{{ $group['group_name'] }}</option>
-                                                                @endforeach
-                                                            </select>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-lg-6">
-                                                        <button type="button" class="btn btn-default m-b-10 m-l-5">Cari</button>
-                                                        <button type="button" class="btn btn-dark m-b-10 m-l-5">Reset</button>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                        </div>
 
-                                        {{-- notif --}}
-                                        @include('template/notif')
-
-                                        <hr>
-                                        <table class="table table-responsive table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th class="text-align text-center">No.</th>
-                                                <th class="text-align text-center">Group</th>
-                                                <th class="text-align text-center">Nama Role</th>
-                                                <th class="text-align text-center">Deskripsi Role</th>
-                                                <th class="text-align text-center"> </th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($result as $rs)
-                                                <tr>
-                                                    <th class="text-align text-center"> {{ $no++ }} </th>
-                                                    <td>{{ $rs['group_name'] }}
-                                                    </td>
-                                                    <td>{{ $rs['role_desc'] }}</td>
-                                                    <td>
-                                                            <a href="{{ site_url('sistem/permission/edit/'.$rs['role_id']) }}" class="btn btn-success btn-rounded m-b-10 m-l-5" title="Edit"><i class="ti-pencil"></i></a>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                    <div class="text-right">
-                                        @if (isset($pagination))
-                                            {!! $pagination !!}
-                                        @endif
-                                    </div>
+                        <form class="col-lg-12 row" action="{{ site_url('sistem/permission/search_process') }}" method="POST">
+                            <div class="col-lg-5">
+                                <select name="nav_id" id="single" class="form-control select2-single">
+                                    <option value='0'>Tidak ada</options>
+                                        @foreach ($rs_group as $group)
+                                    <option value="{{ $group['group_id'] }}">{{ $group['group_name'] }}</option>
+                                    @endforeach
+                                </select>
                             </div>
+                            <div class="col-lg-3">
+                                <button type="submit" name="search" value="tampilkan" class="btn btn-info">Cari</button>
+                                <button type="submit" name="search" value="reset"
+                                    class="btn btn-secondary">Reset</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="table-responsive" style="margin-top:2%">
+                        <table class="table">
+                            <thead class="bg-primary text-white">
+                                <tr>
+                                    <th class="text-align text-center" width="5%">No.</th>
+                                    <th class="text-align text-center" width="25%">Group</th>
+                                    <th class="text-align text-center" width="25%">Nama Role</th>
+                                    <th class="text-align text-center" width="25%">Deskripsi Role</th>
+                                    <th width="10%"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($result as $rs)
+                                <tr>
+                                    <th class="text-align text-center"> {{ $no++ }} </th>
+                                    <td>{{ $rs['group_name'] }}</td>
+                                    <td>{{ $rs['role_nm'] }}</td>
+                                    <td>{{ $rs['role_desc'] }}</td>
+                                    <td>
+                                        <a href="{{ site_url('sistem/permission/edit/'.$rs['role_id']) }}"
+                                            class="btn btn-success btn-rounded m-b-10 m-l-5" title="Edit"><i
+                                                class="ti-pencil"></i> Edit</a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div class="text-right">
+                            @if (isset($pagination))
+                            <ul class="pagination pagination-sm">
+                                <li class="page-item"><a class="page-link" href="#">{!! $pagination !!}</a>
+                                </li>
+                            </ul>
+                            @endif
                         </div>
                     </div>
                 </div>
             </div>
-         </div>
         </div>
     </div>
-    <!-- script untuk js external -->
+</div>
+
+<!-- script untuk js external -->
 @push('ext_js')
 <script>
     $(document).ready(function () {
-        $( ".select2-single" ).select2( {
-				placeholder: placeholder,
-                width: '100%',
-				containerCssClass: ':all:'
-			} );
+        $(".select2-single").select2({
+            width: '100%',
+            containerCssClass: ':all:'
+        });
     });
 </script>
 @endpush
