b2a06591c02de5ad415aa6f5825f69cd0eba5500
lathiifas <ajisanthoshol@gmail.com>
2020-03-05 11:30:11 +0700

    bug fixing

--- application/core/Artdev_Router.php
+++ application/core/Artdev_Router.php
@@ -1,6 +1,6 @@
 <?php
 
-class MY_Router extends CI_Router {
+class Artdev_Router extends CI_Router {
     protected function _set_default_controller() {
 
         if (empty($this->default_controller)) {
